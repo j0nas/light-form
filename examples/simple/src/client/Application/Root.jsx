cc1db01e8b099bb297531826b3eba0efdf58db6e
Jonas Jensen <jonas.jensen@msn.com>
2017-05-01 00:39:28 +0200

    add support for nested objects defined by input field name property

--- examples/simple/src/client/Application/Root.jsx
+++ examples/simple/src/client/Application/Root.jsx
@@ -4,9 +4,11 @@ import './Application.scss';
 
 const Root = () =>
   <div>
-    <Input className="input" name="firstName" placeholder="First name" />
-    <Input className="input" name="middleName" placeholder="Middle name" />
-    <Input className="input" name="lastName" placeholder="Last name" />
+    <Input className="input" name="name.first" placeholder="First name" />
+    <Input className="input" name="name.middle" placeholder="Middle name" />
+    <Input className="input" name="name.last" placeholder="Last name" />
+
+    <Input className="input" name="phoneNumber" type="number" placeholder="Phone number" />
   </div>;
 
 export default Root;
--- src/Input/ducks/Input.js
+++ src/Input/ducks/Input.js
@@ -1,4 +1,6 @@
-export const UPDATE_INPUT_VALUE = 'UPDATE_INPUT_VALUE';
+import set from "lodash.set";
+
+const UPDATE_INPUT_VALUE = 'UPDATE_INPUT_VALUE';
 
 export const changeField = (name, value) => ({
     type: UPDATE_INPUT_VALUE,
@@ -9,7 +11,10 @@ export const changeField = (name, value) => ({
 export default (state = {}, action) => {
     switch (action.type) {
         case UPDATE_INPUT_VALUE:
-            return {...state, [action.name]: action.value};
+            const shouldUpdateNestedProperty = action.name.indexOf('.') > -1;
+            return shouldUpdateNestedProperty
+                ? {...set(state, action.name, action.value)}
+                : {...state, [action.name]: action.value};
 
         default:
             return state;
