f0a9ca2081d4fb16c8b6b38a62c5473e8e4e93b4
Jonas Jensen <jonas.jensen@msn.com>
2017-04-30 18:38:37 +0200

    update refs

--- examples/simple/src/client/Application/Root.jsx
+++ examples/simple/src/client/Application/Root.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Input} from '../../../../../dist/es';
+import { Input } from 'light-form';
 import './Application.scss';
 
 const Root = () =>
--- examples/simple/src/client/reducers/index.js
+++ examples/simple/src/client/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import {Reducer as customer} from '../../../../../dist/es';
+import { Reducer as customer } from 'light-form';
 
 export default combineReducers({
   customer,
