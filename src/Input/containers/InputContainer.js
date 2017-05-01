4b8bad29415f452764b0c120a0550a6e00804f2d
Jonas Jensen <jonas.jensen@msn.com>
2017-05-01 02:03:56 +0200

    add multi-reducer support

--- src/Input/containers/InputContainer.js
+++ src/Input/containers/InputContainer.js
@@ -1,28 +1,40 @@
 import PropTypes from "prop-types";
 import {connect} from "react-redux";
 import Input from "../components/Input";
-import {changeField} from "../ducks/Input";
+import {changeField, createBoundType, UPDATE_INPUT_VALUE} from "../ducks/Input";
+
+const getNamespaceOfField = fieldName =>
+    fieldName.indexOf('.') > -1
+        ? fieldName.split('.')[0]
+        : fieldName;
 
 const InputContainer = connect(
         undefined,
         dispatch => ({
             dispatch,
         }),
-        (state, dispatch, own) => ({
-            // Pass in received props first so props defined here overwrite any preexisting ones.
-            ...own,
-            ...state,
-            value: state[own.name],
-            onChange: event =>
-                dispatch.dispatch(
-                    changeField(
-                        own.name,
-                        event.target.type === 'checkbox'
-                            ? event.target.checked
-                            : event.target.value,
-                    )),
-        }),
-    )(Input);
+    (state, dispatch, own) => ({
+        // Pass in received props first so props defined here overwrite any preexisting ones.
+        ...own,
+        ...state,
+        value: state[own.name],
+        onChange: event => {
+            const value = event.target.type === 'checkbox'
+                ? event.target.checked
+                : event.target.value;
+
+            const namespace = getNamespaceOfField(own.name);
+            const fieldHasNamespace =
+                window.__LIGHT_FORM__ &&
+                window.__LIGHT_FORM__.indexOf(namespace) > -1;
+            const type = fieldHasNamespace ?
+                createBoundType(namespace)
+                : UPDATE_INPUT_VALUE;
+
+            return dispatch.dispatch(changeField(type, own.name, value));
+        },
+    }),
+)(Input);
 
 InputContainer.propTypes = {
     name: PropTypes.string.isRequired,
--- src/Input/ducks/Input.js
+++ src/Input/ducks/Input.js
@@ -1,22 +1,51 @@
 import set from "lodash.set";
 
-const UPDATE_INPUT_VALUE = 'UPDATE_INPUT_VALUE';
+export const UPDATE_INPUT_VALUE = 'UPDATE_INPUT_VALUE';
 
-export const changeField = (name, value) => ({
-    type: UPDATE_INPUT_VALUE,
+export const changeField = (type, name, value) => ({
+    type,
     name,
     value,
 });
 
+const calculateNewState = (state, name, value) => {
+    const isNestedField = name.indexOf('.') > -1;
+    return isNestedField
+        ? {...set(state, name, value)}
+        : {...state, [name]: value};
+};
+
 export default (state = {}, action) => {
     switch (action.type) {
         case UPDATE_INPUT_VALUE:
-            const shouldUpdateNestedProperty = action.name.indexOf('.') > -1;
-            return shouldUpdateNestedProperty
-                ? {...set(state, action.name, action.value)}
-                : {...state, [action.name]: action.value};
+            return calculateNewState(state, action.name, action.value);
 
         default:
             return state;
     }
+};
+
+export const createBoundType = namespace =>
+    (UPDATE_INPUT_VALUE + '.' + namespace);
+
+export const boundReducer = namespace => {
+    if (window) {
+        window.__LIGHT_FORM__ = [...(window.__LIGHT_FORM__ || false), namespace];
+    } else {
+        if (process.env.NODE_ENV !== 'production') {
+            console.error("light-form: window is not defined");
+        }
+    }
+
+    return (state = {}, action) => {
+        const boundType = createBoundType(namespace);
+        switch (action.type) {
+            case boundType:
+                const fieldPathWithoutNamespace = action.name.replace(namespace + '.', '');
+                return calculateNewState(state, fieldPathWithoutNamespace, action.value);
+
+            default:
+                return state;
+        }
+    };
 };
