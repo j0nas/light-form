f8370b9a44fc36a37e14648c0a56b9cad60cf86f
Jonas Jensen <jonas.jensen@msn.com>
2017-05-09 21:57:18 +0200

    add Select component to InputTypes example and restructure view

--- examples/src/client/routes/InputTypes.jsx
+++ examples/src/client/routes/InputTypes.jsx
@@ -1,21 +1,32 @@
 import React from 'react';
-import { Input, TextArea } from '../../../../src';
+import { Input, Select, TextArea } from '../../../../src';
 import StateAsText from '../components/StateAsText';
 
 export default () =>
   <div>
-    <Input name="inputTypes.text" placeholder="Text (default)" />
-    <Input name="inputTypes.number" placeholder="Number" type="number" />
-    <Input name="inputTypes.email" placeholder="Supports all W3 types" type="email" />
-    <Input name="inputTypes.date" type="date" />
-
-    <label htmlFor="trueValue">True</label>
-    <Input type="radio" name="inputTypes.radio" id="trueValue" value="true" />
-    <br />
-    <label htmlFor="falseValue">False</label>
-    <Input type="radio" name="inputTypes.radio" id="falseValue" value="false" />
-    <br />
-    <TextArea name="inputTypes.textArea" placeholder="Text area" />
-
+    <p>
+      <Input name="inputTypes.text" placeholder="Text (default)" />
+      <Input name="inputTypes.number" placeholder="Number" type="number" />
+      <Input name="inputTypes.email" placeholder="Supports all W3 types" type="email" />
+      <Input name="inputTypes.date" type="date" />
+    </p>
+    <p>
+      <label htmlFor="trueValue">True</label>
+      <Input type="radio" name="inputTypes.radio" id="trueValue" value="true" />
+      <br />
+      <label htmlFor="falseValue">False</label>
+      <Input type="radio" name="inputTypes.radio" id="falseValue" value="false" />
+    </p>
+    <p>
+      <TextArea name="inputTypes.textArea" placeholder="Text area" />
+    </p>
+    <p>
+      <Select name="inputTypes.select">
+        <option value="default">Select one</option>
+        <option value="one">One</option>
+        <option value="two">Two</option>
+        <option value="three">Three</option>
+      </Select>
+    </p>
     <StateAsText nodeName="inputTypes" />
   </div>;
