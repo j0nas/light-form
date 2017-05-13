853e8a01569de169da8dc16b65842ccb043fb6de
Jonas Jensen <jonas.jensen@msn.com>
2017-05-13 10:55:26 +0200

    complete onChange decoration test

--- test/Input/containers/InputContainer.test.js
+++ test/Input/containers/InputContainer.test.js
@@ -1,35 +1,24 @@
 import React from 'react';
-import {shallow, mount} from 'enzyme';
-import InputContainer from "../../../src/Input/containers/InputContainer";
+import {mount} from 'enzyme';
 import Input from "../../../src/Input/components/Input";
-
-
-// TODO verify this:
-// TODO container passes onChange
-// TODO container correctly validates props
+import InputContainer from "../../../src/Input/containers/InputContainer";
+import ConfiguredProvider from '../../util/fakes/ConfiguredProvider';
 
 describe('InputContainer', () => {
   it('decorates provided components with an onChange', () => {
-    const inputComponent = <input type="text" />;
-    const container = mount(inputComponent);
+    const InputComponent = <Input name='group.field'/>;
+    const inputWrapper = mount(InputComponent);
 
-    expect(container.prop('type')).toBe('text');
-    expect(container.prop('onChange')).toBeUndefined();
+    expect(inputWrapper.find('input').prop('onChange')).toBeUndefined();
 
-    const containerContainer = InputContainer(<Input name="lol.kek"/>);
-    const contain = mount(<containerContainer id="wrapperwrapper" />);
-    const ctr = contain.find("#wrapperwrapper");
-    const data = ctr;
+    const InputContainerComponent = InputContainer(Input);
+    const containerWrapper = mount(
+      <ConfiguredProvider>
+        <InputContainerComponent name="group.name"/>
+      </ConfiguredProvider>
+    );
 
-
-    expect(data.prop('type')).toBe('text');
-    //expect(container.prop('onChange')).toBeUndefined();
-
-
-    //const shallowContainer = shallow());
-    //const inputContainer = <InputContainer/>;
-    //expect(containerContainer.prop('type')).toBe('text');
-    //expect(shallowContainer.prop('onChange')).toBeUndefined();
+    expect(containerWrapper.find('input').prop('onChange')).not.toBeUndefined();
   });
 });
 
