74ecc68a63c7f7cacbff9da3bbc1c58337e06d28
Jonas Jensen <jonas.jensen@msn.com>
2017-05-13 18:20:34 +0200

    test onChange for components plus checkbox-specific test

--- test/Input/containers/InputContainer.test.js
+++ test/Input/containers/InputContainer.test.js
@@ -1,8 +1,9 @@
 import React from "react";
 import {mount} from "enzyme";
+import {Input as ExportedInput} from "../../../src";
 import Input from "../../../src/Input/components/Input";
 import InputContainer from "../../../src/Input/containers/InputContainer";
-import ConfiguredProvider from "../../util/fakes/ConfiguredProvider";
+import ConfiguredProvider, {store} from "../../util/fakes/ConfiguredProvider";
 
 describe('InputContainer', () => {
   it('decorates provided components with an onChange', () => {
@@ -19,5 +20,37 @@ describe('InputContainer', () => {
     );
 
     expect(containerWrapper.find('input').prop('onChange')).not.toBeUndefined();
+  });
+
+  it('dispatches updates when it is changed', () => {
+    const wrapper = mount(
+      <ConfiguredProvider customStore={store}>
+        <ExportedInput name="test.name"/>
+      </ConfiguredProvider>,
+    );
+
+    const input = wrapper.find('input').first();
+    input.simulate('change', {target: {name: 'test.name', value: 'test'}});
+    expect(store.getState()).toEqual(expect.objectContaining({test: {name: 'test'}}));
+  });
+
+  it('returns a boolean when input with type=checkbox is changed', () => {
+    const wrapper = mount(
+      <ConfiguredProvider customStore={store}>
+        <ExportedInput name="test.checkbox" type="checkbox"/>
+      </ConfiguredProvider>,
+    );
+
+    const input = wrapper.find('input').first();
+    expect(input.prop('checked')).toBeFalsy();
+
+    const mockCheckboxEvent = value =>
+      ({target: {name: 'test.checkbox', type:'checkbox', checked: value}});
+
+    input.simulate('change', mockCheckboxEvent(true));
+    expect(store.getState().test.checkbox).toEqual(true);
+
+    input.simulate('change', mockCheckboxEvent(false));
+    expect(store.getState().test.checkbox).toEqual(false);
   });
 });
