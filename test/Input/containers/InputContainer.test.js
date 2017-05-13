f00996bad29e305be3334a1819f374f74de3253f
Jonas Jensen <jonas.jensen@msn.com>
2017-05-13 11:52:08 +0200

    test name prop validation

--- test/Input/containers/InputContainer.test.js
+++ test/Input/containers/InputContainer.test.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import {mount} from 'enzyme';
+import React from "react";
+import {mount} from "enzyme";
 import Input from "../../../src/Input/components/Input";
-import InputContainer from "../../../src/Input/containers/InputContainer";
-import ConfiguredProvider from '../../util/fakes/ConfiguredProvider';
+import InputContainer, {validateNameProp} from "../../../src/Input/containers/InputContainer";
+import ConfiguredProvider from "../../util/fakes/ConfiguredProvider";
 
 describe('InputContainer', () => {
   it('decorates provided components with an onChange', () => {
@@ -15,10 +15,26 @@ describe('InputContainer', () => {
     const containerWrapper = mount(
       <ConfiguredProvider>
         <InputContainerComponent name="group.name"/>
-      </ConfiguredProvider>
+      </ConfiguredProvider>,
     );
 
     expect(containerWrapper.find('input').prop('onChange')).not.toBeUndefined();
   });
-});
 
+  it('accepts valid props', () => {
+    const validProps = validateNameProp({name: 'validGroup.validName'}, 'name', 'dummyComponent');
+    expect(validProps).toBeUndefined();
+  });
+
+  it('returns expected error on missing name property', () => {
+    const invalidProps = validateNameProp({}, 'name', 'dummyComponent');
+    expect(invalidProps).toBeInstanceOf(Error);
+    expect(invalidProps.message).toContain('Value must be a valid string.');
+  });
+
+  it('returns expected error on invalid name property', () => {
+    const invalidProps = validateNameProp({name: 'thisIsNotADotDelimitedString'}, 'name', 'dummyComponent');
+    expect(invalidProps).toBeInstanceOf(Error);
+    expect(invalidProps.message).toContain('Value must contain a dot-delimited namespace.');
+  });
+});
