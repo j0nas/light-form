7dd981acfb7bbed170773899a2e16ba160d51791
Jonas Jensen <jonas.jensen@msn.com>
2017-05-14 10:39:42 +0200

    validate onChange prop to be of type function + test

--- src/Input/containers/InputContainer.proptypes.js
+++ src/Input/containers/InputContainer.proptypes.js
@@ -1,14 +1,29 @@
+const errorMessage = (propName, componentName, customMessage) =>
+  (`Invalid property '${propName}' supplied to '${componentName}'. ${customMessage}`);
+
 const validateNameProp = (props, propName, componentName) => {
-  const error = "Invalid property 'name' supplied to '" + componentName + "'. ";
   if (typeof props[propName] !== 'string') {
-    return new Error(error + "Value must be a valid string.");
+    return new Error(errorMessage(propName, componentName, 'Value must be a valid string.'));
   }
 
   if (!/.+\..+/.test(props[propName])) {
-    return new Error(error + "Value must contain a dot-delimited namespace. (eg. name=\"customer.firstname\")");
+    return new Error(errorMessage(
+      propName,
+      componentName,
+      'Value must contain a dot-delimited namespace. (eg. name="customer.firstname")'
+    ));
   }
+};
+
+const validateOnChangeProp = (props, propName, componentName) => {
+  if (typeof props[propName] !== 'function') {
+    return new Error(errorMessage(propName, componentName, 'Value must be a function.'));
+  }
+
+  // TODO test that onChange return an event
 };
 
 export default {
   name: validateNameProp,
+  onChange: validateOnChangeProp,
 }
--- test/Input/containers/InputContainer.proptypes.test.js
+++ test/Input/containers/InputContainer.proptypes.test.js
@@ -2,8 +2,11 @@ import propTypes from "../../../src/Input/containers/InputContainer.proptypes";
 
 describe('InputContainer propTypes', () => {
   it('accepts valid props', () => {
-    const validProps = propTypes.name({name: 'validGroup.validName'}, 'name', 'dummyComponent');
-    expect(validProps).toBeUndefined();
+    const validNameProp = propTypes.name({name: 'validGroup.validName'}, 'name', 'dummyComponent');
+    expect(validNameProp).toBeUndefined();
+
+    const validOnChangeProp = propTypes.onChange({onChange: e => e}, 'onChange', 'dummyComponent');
+    expect(validOnChangeProp).toBeUndefined();
   });
 
   it('returns expected error on missing name property', () => {
@@ -16,5 +19,11 @@ describe('InputContainer propTypes', () => {
     const invalidProps = propTypes.name({name: 'thisIsNotADotDelimitedString'}, 'name', 'dummyComponent');
     expect(invalidProps).toBeInstanceOf(Error);
     expect(invalidProps.message).toContain('Value must contain a dot-delimited namespace.');
+  });
+
+  it('returns expected error on invalid onChange property if present', () => {
+    const invalidProps = propTypes.onChange({onCHange: 'thisIsNotAFunction'}, 'onChange', 'dummyComponent');
+    expect(invalidProps).toBeInstanceOf(Error);
+    expect(invalidProps.message).toContain('Value must be a function.');
   });
 });
