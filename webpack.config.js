726b9a4584acc51ffa2239396bc0c0db21f4f214
Jonas Jensen <jonasj@mesan.no>
2017-05-20 23:41:38 +0200

    webpack config for building minified umd bundle

--- webpack.config.js
+++ webpack.config.js
@@ -3,53 +3,51 @@ const webpack = require("webpack");
 
 const srcDir = __dirname;
 
-const uglifyJsPluginConfig = {
-    compress: {
-        warnings: false,
-        screw_ie8: true,
-        conditionals: true,
-        unused: true,
-        comparisons: true,
-        sequences: true,
-        dead_code: true,
-        evaluate: true,
-        if_return: true,
-        join_vars: true,
-    },
-    mangle: {
-        screw_ie8: true
-    },
-    output: {
-        comments: false,
-        screw_ie8: true,
-    },
-    sourceMap: false,
-};
-
 module.exports = {
-    devtool: false,
-    bail: true,
-    entry: path.join(srcDir, 'src', 'index.js'),
-    output: {
-        path: path.join(srcDir, 'dist'),
-        filename: 'light-form.min.js',
-    },
-    resolve: {
-        extensions: ['.js', '.jsx'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                include: path.join(srcDir, 'src'),
-                loader: 'babel-loader',
-            },
-        ],
-    },
-    plugins: [
-        new webpack.NoEmitOnErrorsPlugin(),
-        new webpack.DefinePlugin({'process.env.NODE_ENV': '"production"'}),
-        new webpack.LoaderOptionsPlugin({minimize: true, debug: false}),
-        new webpack.optimize.UglifyJsPlugin(uglifyJsPluginConfig),
+  devtool: false,
+  bail: true,
+  entry: path.join(srcDir, 'src', 'index.js'),
+  output: {
+    path: path.join(srcDir, 'dist'),
+    filename: 'light-form.min.js',
+    libraryTarget: "umd",
+    library: "light-form",
+  },
+
+  externals: {
+    "react": "React",
+    "react-dom": "ReactDOM",
+    "react-redux": "ReactRedux",
+    "redux": "Redux",
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: path.join(srcDir, 'src'),
+        loader: 'babel-loader',
+        options: {
+          presets: [["es2015", {modules: false}], ["react"]],
+          plugins: [
+            [
+              "transform-react-remove-prop-types",
+              {
+                removeImport: true,
+              }
+            ],
+          ]
+        }
+      },
     ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({'process.env.NODE_ENV': '"production"'}),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.LoaderOptionsPlugin({minimize: true, debug: false}),
+    new webpack.optimize.UglifyJsPlugin(),
+  ],
 };
