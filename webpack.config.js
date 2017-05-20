d6b3b89a55d385787fdb279ea510cac58d98ca1f
Jonas Jensen <jonasj@mesan.no>
2017-05-20 23:48:48 +0200

    removed unnecessary options

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,6 @@ const webpack = require("webpack");
 const srcDir = __dirname;
 
 module.exports = {
-  devtool: false,
-  bail: true,
   entry: path.join(srcDir, 'src', 'index.js'),
   output: {
     path: path.join(srcDir, 'dist'),
@@ -13,14 +11,12 @@ module.exports = {
     libraryTarget: "umd",
     library: "light-form",
   },
-
   externals: {
     "react": "React",
     "react-dom": "ReactDOM",
     "react-redux": "ReactRedux",
     "redux": "Redux",
   },
-
   resolve: {
     extensions: ['.js', '.jsx'],
   },
@@ -32,22 +28,13 @@ module.exports = {
         loader: 'babel-loader',
         options: {
           presets: [["es2015", {modules: false}], ["react"]],
-          plugins: [
-            [
-              "transform-react-remove-prop-types",
-              {
-                removeImport: true,
-              }
-            ],
-          ]
+          plugins: [["transform-react-remove-prop-types", {removeImport: true}]]
         }
       },
     ],
   },
   plugins: [
     new webpack.DefinePlugin({'process.env.NODE_ENV': '"production"'}),
-    new webpack.NoEmitOnErrorsPlugin(),
-    new webpack.LoaderOptionsPlugin({minimize: true, debug: false}),
     new webpack.optimize.UglifyJsPlugin(),
   ],
 };
