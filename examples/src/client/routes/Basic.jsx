bfece384e9528c42bd2d80872b7c898b7e3ad5df
Jonas Jensen <jonas.jensen@msn.com>
2017-05-21 12:41:13 +0200

    fix aXe warnings

--- examples/src/client/routes/Basic.jsx
+++ examples/src/client/routes/Basic.jsx
@@ -4,9 +4,20 @@ import StateAsText from '../components/StateAsText';
 
 export default () =>
   (<div>
-    <Input name="basic.firstname" placeholder="First name" />
-    <Input name="basic.middlename" placeholder="Middle name" />
-    <Input name="basic.lastname" placeholder="Last name" />
+    <label htmlFor="basic.firstname">
+      First name
+      <Input name="basic.firstname" />
+    </label>
+    <br />
+    <label htmlFor="basic.middlename">
+      Middle name
+      <Input name="basic.middlename" />
+    </label>
+    <br />
+    <label htmlFor="basic.lastname">
+      Last name
+      <Input name="basic.lastname" />
+    </label>
 
     <StateAsText nodeName="basic" />
   </div>);
