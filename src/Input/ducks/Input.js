092e2107ea23aac37af9254afb267ea2b5e5cc68
Jonas Jensen <jonas.jensen@greenbird.com>
2017-07-06 08:48:51 +0200

    add new 'customHandlers' parameter to Input reducer that allows adding custom reducer action handlers

--- src/Input/ducks/Input.js
+++ src/Input/ducks/Input.js
@@ -11,16 +11,21 @@ export const changeField = (type, name, value) => ({
 export const createBoundType = namespace =>
     (UPDATE_INPUT_VALUE + '.' + namespace);
 
-export default (namespace, defaultState, onStateChange) =>
+export default (namespace, defaultState, onStateChange, actionHandlers) =>
     (state = defaultState || {}, action) => {
         const boundType = createBoundType(namespace);
-        switch (action.type) {
-            case boundType:
+
+        const reducer = {
+            [boundType]: () => {
                 const fieldPathWithoutNamespace = action.name.replace(namespace + '.', '');
                 const newState = dotProp.set(state, fieldPathWithoutNamespace, action.value);
                 return onStateChange && onStateChange(newState) || newState;
+            },
 
-            default:
-                return state;
-        }
+            ...actionHandlers,
+        };
+
+        return reducer[action.type]
+            ? reducer[action.type]()
+            : state;
     };
